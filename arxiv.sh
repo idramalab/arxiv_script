384b460ee2e4b351ac7e2ebb66a8f2a0562dc568
emidec <emidec@gmail.com>
2018-04-14 10:38:31 +0100

    removing buggy figure extraction

--- arxiv.sh
+++ arxiv.sh
@@ -2,22 +2,15 @@
 
 ##### INPUT #####
 ##Modify this accordingly
-#main source file
-main="main"
-#bib file
-bib="biblio"
-#folder with figures
-fig_f="figures/"
+main="main"  #main source file
+bib="biblio" #bib file 
+fig_f="figures/" #folder with figures
 
 ##### OUTPUT #####
 ##Modify this with your preferred file names
-#tex file without comments
-upload="no_comments"
-#compress files so you can upload a single tar
-archive="all.tar"
+upload="no_comments" #tex file without comments
+archive="all.tar" #compress files so you can upload a single tar
 
-##If only minor changes to main
-minor=0
 
 #Removing all comments from the source
 perl -pe 's/(^|[^\\])%.*/\1%/' < $main.tex > $upload.tex
@@ -36,25 +29,12 @@ perl -pi -e "s/$s/$r/g" $upload.tex
 ##Remove Copyright
 perl -pi -e 's/\\begin{document}/\\makeatletter\n\\def\\\@copyrightspace{\\relax}\n\\makeatother\n\\begin{document} /g' $upload
 
-##Get the list of figures so you can remove unnecessary ones from the figures folder
-if [ "$minor" -ne 1 ]
-then
-cat $upload.tex | grep includegraphics | awk -F"[{}]" '{print $2}' | tar -cvf temp.tar -T -
-tar cf - $fig_f/ | xz -9 -zf - > _figs_backup.tar.xz
-rm -rf $fig_f
-tar xf temp.tar
-rm -rf temp.tar
-fi
-
 ##Final compilation
 pdflatex $upload.tex
 pdflatex $upload.tex
 
 ##Creating an archive with (hopefully) all files
-if [ "$minor" -ne 1 ]
-then
 tar -cvzf $archive $upload.tex $upload.bbl $fig_f *.cls
-fi
 
 ##Cleaning up
 rm -f *~
